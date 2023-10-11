37a8b6c537fcfa246612d2b8b4445385658e6601
adam-brannon09 <adam.brannon09@gmail.com>
2023-10-11 08:28:03 -0500

    added custom hook

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
 
 function Search() {
 
     const [city, setCity] = useState("")
-
     const handleChange = (e) => setCity(e.target.value)
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const apiKey = '202feb99a1218137bb0b3c379ff2a746'
+    let coordinatesUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+
+
+
+
+
 
     return (
         <div>
